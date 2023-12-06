02e844bb2af72c346a2c3b3978919f6417988171
Dan <santos.danielfd@gmail.com>
2023-12-06 18:30:02 -0300

    added use cases as providers of http module

--- courier-api/src/infra/database/prisma/repositories/prisma-couriers.repository.ts
+++ courier-api/src/infra/database/prisma/repositories/prisma-couriers.repository.ts
@@ -1,7 +1,17 @@
+import { PaginationParams } from 'src/core/pagination-params';
 import { Courier } from 'src/domain/couriers/entities/courier.entity';
 import { ICouriersRepository } from 'src/domain/couriers/repositories/couriers.repository'
 
 export class PrismaCouriersRepository implements ICouriersRepository {
+  save(courier: Courier): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  findMany(params: PaginationParams): Promise<Courier[]> {
+    throw new Error('Method not implemented.');
+  }
   create(courier: Courier): Promise<void> {
     throw new Error('Method not implemented.');
   }
--- courier-api/src/infra/http/http.module.ts
+++ courier-api/src/infra/http/http.module.ts
@@ -2,13 +2,21 @@ import { Module } from '@nestjs/common';
 import { CouriersController } from './controllers/couriers.controller';
 import { CreateUseCase } from 'src/domain/couriers/use-cases/create.use-case';
 import { DatabaseModule } from '../database/database.module';
+import { UpdateUseCase } from 'src/domain/couriers/use-cases/update.use-case';
+import { DeleteUseCase } from 'src/domain/couriers/use-cases/delete.use-case';
+import { FindByIdUseCase } from 'src/domain/couriers/use-cases/find-by-id.use-case';
+import { FindManyUseCase } from 'src/domain/couriers/use-cases/find-many.use-case';
 
 @Module({
   controllers: [
     CouriersController
   ],
   providers: [
-    CreateUseCase
+    CreateUseCase,
+    UpdateUseCase,
+    DeleteUseCase,
+    FindByIdUseCase,
+    FindManyUseCase
   ],
   imports: [
     DatabaseModule

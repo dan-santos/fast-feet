cb13f67fe7f5bebd4014ee738e6674ec73f545c8
Dan <santos.danielfd@gmail.com>
2023-12-06 22:09:51 -0300

    added v0 of swagger documentation

--- courier-api/src/main.ts
+++ courier-api/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from '@env/env.service';
 import { ErrorHandlerInterceptor } from './error-handler/error-handler.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const env = app.get(EnvService);
   app.useGlobalInterceptors(new ErrorHandlerInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Fast Feet\'s Couriers API')
+    .setDescription('API for managing couriers that provides shippings for users')
+    .setVersion('0.1.0')
+    .addTag('couriers')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(env.get('PORT'));
 }
 bootstrap();

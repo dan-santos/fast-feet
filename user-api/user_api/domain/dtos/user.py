9bd20e0e24fb30670269dcfbdff6a63b5d8c8d4c
Dan <santos.danielfd@gmail.com>
2024-01-23 14:31:11 -0300

    removed user-api mvp with fastapi

--- user-api/user_api/domain/dtos/user.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-from user_api.domain.models.user import Roles
-
-class CreateUserRequest(BaseModel):
-    email: EmailStr
-    full_name: str
-    password: str
-    role: Roles
-    is_active: bool = False
